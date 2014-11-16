0f09ebf99c3dd5eb814bde04ea9d9831b30827f7
Erik van Zijst <erik.van.zijst@gmail.com>
2014-11-16 00:01:29 -0800

    Fixed doco type.

--- vsqs/queue.py
+++ vsqs/queue.py
@@ -62,7 +62,7 @@ class Queue(object):
             self._mid_cache.popitem(last=False)
 
     def size(self):
-        """Returns the number of messages currently in the queue. Not that this
+        """Returns the number of messages currently in the queue. Note that this
         includes message that are being consumed, but have not yet been deleted.
         """
         return sum(1 for m in self._list_messages())
